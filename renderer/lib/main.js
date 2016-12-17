04cb2dcf77134acc0af50e7f5b028d527bb790dd
Markus Peröbner <markus.peroebner@gmail.com>
2016-12-17 17:01:35 +0100

    refactores site definition and page definition loading
    now site defintions and page defintions are assigned to the Renderer
    object. this prevents passing them with every function call.

--- renderer/lib/main.js
+++ renderer/lib/main.js
@@ -8,6 +8,25 @@ const q = require('q');
 function renderSite(outputDirPath, siteDefinitionPath){
     return q.when()
         .then(function(){
+            return new Renderer(outputDirPath, siteDefinitionPath).renderSite();
+        });
+}
+
+function Renderer(outputDir, siteDefinitionPath){
+    this.outputDir = outputDir;
+    this._siteDefinition = this._getSiteDefinition(siteDefinitionPath);
+    /**
+     * Key is the page JSON's path. Value is a promise which resolves to
+     * the page object which is the parsed page JSON. May also contain
+     * pages for the articles, footer or any other section which
+     * contains pages.
+     */
+    this._pageDefinitions = new Map();
+}
+
+Renderer.prototype._getSiteDefinition = function _getSiteDefinition(siteDefinitionPath){
+    return q.when()
+        .then(function(){
             return fs.read(siteDefinitionPath);
         })
         .then(function(siteDefinitionTxt){
@@ -15,38 +34,33 @@ function renderSite(outputDirPath, siteDefinitionPath){
             if(!siteDefinition.basePath){
                 siteDefinition.basePath = path.dirname(siteDefinitionPath);
             }
-            const renderer = new Renderer(outputDirPath);
-            return renderer.renderSite(siteDefinition);
+            return siteDefinition;
         });
-}
-
-function Renderer(outputDir){
-    this.outputDir = outputDir;
-}
-
-Renderer.prototype.renderSite = function renderSite(siteDefinition){
-    const renderer = this;
-    return q.all([]
-                 .concat(siteDefinition.pages.map(p => renderer.loadAndRenderPage(siteDefinition, p)))
-                 .concat(siteDefinition.articles.map(a => renderer.loadAndRenderPage(siteDefinition, a)))
-                 .concat(siteDefinition.footer.map(f => renderer.loadAndRenderPage(siteDefinition, f)))
-                );
 };
 
-Renderer.prototype.loadAndRenderPage = function loadAndRenderPage(siteDefinition, pageDefinitionPath){
+Renderer.prototype._getPageDefinition = function _getPageDefinition(pageDefinitionPath){
     const renderer = this;
-    let fullPageDefinitionPath;
-    return q.when()
-        .then(function(){
-            fullPageDefinitionPath = path.join(siteDefinition.basePath, pageDefinitionPath);
-            return loadDefinition(fullPageDefinitionPath);
-        })
-        .then(function(pageDefinition){
-            if(!pageDefinition.basePath){
-                pageDefinition.basePath = path.dirname(fullPageDefinitionPath);
-            }
-            return renderer.renderPage(siteDefinition, pageDefinition, siteDefinition.pages[0] === pageDefinitionPath);
-        });
+    if(renderer._pageDefinitions.has(pageDefinitionPath)){
+        return renderer._pageDefinitions.get(pageDefinitionPath);
+    }
+    let siteDefinition;
+    const pageLoadPromise = q.when()
+          .then(function(){
+              return renderer._siteDefinition;
+          })
+          .then(function(_siteDefinition_){
+              siteDefinition = _siteDefinition_;
+              fullPageDefinitionPath = path.join(siteDefinition.basePath, pageDefinitionPath);
+              return loadDefinition(fullPageDefinitionPath);
+          })
+          .then(function(pageDefinition){
+              if(!pageDefinition.basePath){
+                  pageDefinition.basePath = path.dirname(fullPageDefinitionPath);
+              }
+              return pageDefinition;
+          });
+    this._pageDefinitions.set(pageDefinitionPath, pageLoadPromise);
+    return pageLoadPromise;
 };
 
 function loadDefinition(definitionPath){
@@ -63,26 +77,45 @@ function loadDefinition(definitionPath){
         });
 }
 
-Renderer.prototype.renderPage = function renderPage(siteDefiniton, pageDefinition, firstPage){
+Renderer.prototype.renderSite = function renderSite(){
     const renderer = this;
-    let pageDirectoryPath;
     return q.when()
         .then(function(){
-            return renderer._buildPageDirectory(siteDefiniton, pageDefinition, firstPage);
+            return renderer._siteDefinition;
+        })
+        .then(function(siteDefinition){
+            return q.all([]
+                         .concat(siteDefinition.pages.map((p, i) => renderer.renderPage(p, i === 0)))
+                         .concat(siteDefinition.articles.map(a => renderer.renderPage(a, false)))
+                         .concat(siteDefinition.footer.map(f => renderer.renderPage(f, false)))
+                        );
+        });
+};
+
+Renderer.prototype.renderPage = function renderPage(pagePath, firstPage){
+    const renderer = this;
+    let pageDirectoryPath, siteDefinition, pageDefinition;
+    return q.when()
+        .then(function(){
+            return renderer._getPageDefinition(pagePath);
+        })
+        .then(function(_pageDefinition_){
+            pageDefinition = _pageDefinition_;
+            return renderer._buildPageDirectory(pageDefinition, firstPage);
         })
         .then(function(_pageDirectoryPath_){
             pageDirectoryPath = _pageDirectoryPath_;
             let promises = [
                 q.when('<!DOCTYPE html>\n<html>'),
-                renderer.renderPageHead(pageDirectoryPath, siteDefiniton, pageDefinition),
+                renderer.renderPageHead(pageDirectoryPath, pageDefinition),
                 q.when('<body><div class="ep-container">'),
-                renderer.renderPageNavigation(pageDirectoryPath, siteDefiniton, pageDefinition),
+                renderer.renderPageNavigation(pageDirectoryPath, pageDefinition),
                 q.when('<div class="ep-content">'),
             ];
-            promises = promises.concat(pageDefinition.content.map(c => renderer.renderPageContentSegment(pageDirectoryPath, siteDefiniton, pageDefinition, c)));
+            promises = promises.concat(pageDefinition.content.map(c => renderer.renderPageContentSegment(pageDirectoryPath, pageDefinition, c)));
             promises = promises.concat([
                 q.when('</div>'),
-                renderer.renderFooter(pageDirectoryPath, siteDefiniton, pageDefinition),
+                renderer.renderFooter(pageDirectoryPath, pageDefinition),
                 q.when('</div></body></html>'),
             ]);
             return q.all(promises);
@@ -95,12 +128,12 @@ Renderer.prototype.renderPage = function renderPage(siteDefiniton, pageDefinitio
         });
 };
 
-Renderer.prototype._buildPageDirectory = function(siteDefiniton, pageDefinition, firstPage){
+Renderer.prototype._buildPageDirectory = function(pageDefinition, firstPage){
     const renderer = this;
     let pageDirectoryPath;
     return q.when()
         .then(function(){
-            return renderer._getPageDirectoryName(siteDefiniton, pageDefinition, firstPage);
+            return renderer._getPageDirectoryName(pageDefinition, firstPage);
         })
         .then(function(pageDirectoryName){
             pageDirectoryPath = path.join(renderer.outputDir, pageDirectoryName);
@@ -111,7 +144,7 @@ Renderer.prototype._buildPageDirectory = function(siteDefiniton, pageDefinition,
         });
 };
 
-Renderer.prototype._getPageDirectoryName = function(siteDefiniton, pageDefinition, firstPage){
+Renderer.prototype._getPageDirectoryName = function(pageDefinition, firstPage){
     const renderer = this;
     if(firstPage){
         /*
@@ -158,25 +191,29 @@ function urlifyLabel(label){
     return urlifiedLabelChars.join('');
 }
 
-Renderer.prototype.renderPageHead = function renderPageHead(outputDirPath, siteDefiniton, pageDefinition){
+Renderer.prototype.renderPageHead = function renderPageHead(outputDirPath, pageDefinition){
+    const renderer = this;
     return q.when()
         .then(function(){
+            return renderer._siteDefinition;
+        })
+        .then(function(siteDefinition){
             let htmlArtifacts = [
-                `<head><meta charset="UTF-8"/><title>${pageDefinition.title} - ${siteDefiniton.title}</title>`,
+                `<head><meta charset="UTF-8"/><title>${pageDefinition.title} - ${siteDefinition.title}</title>`,
             ];
-            htmlArtifacts = htmlArtifacts.concat(siteDefiniton.stylesheets.map(s => `<link rel="stylesheet" type="text/css" href="${s}"/>`));
+            htmlArtifacts = htmlArtifacts.concat(siteDefinition.stylesheets.map(s => `<link rel="stylesheet" type="text/css" href="${s}"/>`));
             htmlArtifacts.push('</head>');
             return htmlArtifacts.join('');
         });
 };
 
-Renderer.prototype.renderPageNavigation = function renderPageNavigation(outputDirPath, siteDefiniton, pageDefinition){
+Renderer.prototype.renderPageNavigation = function renderPageNavigation(outputDirPath, pageDefinition){
     const renderer = this;
     return q.when()
         .then(function(){
             return q.all([
                 q.when('<nav class="ep-page-navigation"><ul>'),
-                renderer.renderPageNavigationLinks(outputDirPath, siteDefiniton, pageDefinition),
+                renderer.renderPageNavigationLinks(outputDirPath, pageDefinition),
                 q.when('</ul></nav>'),
             ]);
         })
@@ -185,16 +222,20 @@ Renderer.prototype.renderPageNavigation = function renderPageNavigation(outputDi
         });
 };
 
-Renderer.prototype.renderPageNavigationLinks = function renderPageNavigationLinks(outputDirPath, siteDefinition, activePageDefinition){
+Renderer.prototype.renderPageNavigationLinks = function renderPageNavigationLinks(outputDirPath, activePageDefinition){
     const renderer = this;
-    let pageDefinitions;
+    let siteDefinition, pageDefinitions;
     return q.when()
         .then(function(){
-            return q.all(siteDefinition.pages.map(pageDefinitionPath => loadDefinition(path.join(siteDefinition.basePath, pageDefinitionPath))));
+            return renderer._siteDefinition;
+        })
+        .then(function(_siteDefinition_){
+            siteDefinition = _siteDefinition_;
+            return q.all(siteDefinition.pages.map(pageDefinitionPath => renderer._getPageDefinition(pageDefinitionPath)));
         })
         .then(function(_pageDefinitions_){
             pageDefinitions = _pageDefinitions_;
-            return q.all(pageDefinitions.map((pd, i) => renderer._getPageDirectoryName(siteDefinition, pd, i === 0)));
+            return q.all(pageDefinitions.map((pd, i) => renderer._getPageDirectoryName(pd, i === 0)));
         })
         .then(function(pageDirectoryNames){
             return pageDefinitions.map((pd, i) => `<li><a href="/${pageDirectoryNames[i]}">${escapeHtml(pd.title)}</a></li>`)
@@ -202,7 +243,7 @@ Renderer.prototype.renderPageNavigationLinks = function renderPageNavigationLink
         });
 };
 
-Renderer.prototype.renderPageContentSegment = function renderPageContentSegment(outputDirPath, siteDefiniton, pageDefinition, content){
+Renderer.prototype.renderPageContentSegment = function renderPageContentSegment(outputDirPath, pageDefinition, content){
     const renderer = this;
     return q.when()
         .then(function(){
@@ -210,29 +251,33 @@ Renderer.prototype.renderPageContentSegment = function renderPageContentSegment(
             default:
                 return q.reject(new Error('ep.unknownContentSegmentType: ' + content.type));
             case 'articles-archive':
-                return renderer._renderPageContentSegmentArticlesArchive(outputDirPath, siteDefiniton, pageDefinition, content);
+                return renderer._renderPageContentSegmentArticlesArchive(outputDirPath, pageDefinition, content);
             case 'headline':
-                return renderer._renderPageContentSegmentHeadline(outputDirPath, siteDefiniton, pageDefinition, content);
+                return renderer._renderPageContentSegmentHeadline(outputDirPath, pageDefinition, content);
             case 'image':
-                return renderer._renderPageContentSegmentImage(outputDirPath, siteDefiniton, pageDefinition, content);
+                return renderer._renderPageContentSegmentImage(outputDirPath, pageDefinition, content);
             case 'lines':
-                return renderer._renderPageContentSegmentLines(outputDirPath, siteDefiniton, pageDefinition, content);
+                return renderer._renderPageContentSegmentLines(outputDirPath, pageDefinition, content);
             case 'paragraph':
-                return renderer._renderPageContentSegmentParagraph(outputDirPath, siteDefiniton, pageDefinition, content);
+                return renderer._renderPageContentSegmentParagraph(outputDirPath, pageDefinition, content);
             }
         });
 };
 
-Renderer.prototype._renderPageContentSegmentArticlesArchive = function _renderPageContentSegmentArticlesArchive(outputDirPath, siteDefinition, pageDefinition, content){
+Renderer.prototype._renderPageContentSegmentArticlesArchive = function _renderPageContentSegmentArticlesArchive(outputDirPath, pageDefinition, content){
     const renderer = this;
     let articleDefinitions;
     return q.when()
         .then(function(){
-            return q.all(siteDefinition.articles.map(articleDefinitionPath => loadDefinition(path.join(siteDefinition.basePath, articleDefinitionPath))));
+            return renderer._siteDefinition;
+        })
+        .then(function(_siteDefinition_){
+            siteDefinition = _siteDefinition_;
+            return q.all(siteDefinition.articles.map(articleDefinitionPath => renderer._getPageDefinition(articleDefinitionPath)));
         })
         .then(function(_articleDefinitions_){
             articleDefinitions = _articleDefinitions_;
-            return q.all(articleDefinitions.map((ad, i) => renderer._getPageDirectoryName(siteDefinition, ad, false)));
+            return q.all(articleDefinitions.map(ad => renderer._getPageDirectoryName(ad, false)));
         })
         .then(function(articleDirectoryNames){
             let htmlFragments = [
@@ -244,11 +289,11 @@ Renderer.prototype._renderPageContentSegmentArticlesArchive = function _renderPa
         });
 };
 
-Renderer.prototype._renderPageContentSegmentHeadline = function _renderPageContentSegmentHeadline(outputDirPath, siteDefiniton, pageDefinition, content){
+Renderer.prototype._renderPageContentSegmentHeadline = function _renderPageContentSegmentHeadline(outputDirPath, pageDefinition, content){
     return `<h1 class="ep-headline">${escapeHtml(content.text)}</h1>`;
 };
 
-Renderer.prototype._renderPageContentSegmentImage = function _renderPageContentSegmentImage(outputDirPath, siteDefiniton, pageDefinition, content){
+Renderer.prototype._renderPageContentSegmentImage = function _renderPageContentSegmentImage(outputDirPath, pageDefinition, content){
     let renderedImagePath;
     return q.when()
         .then(function(){
@@ -260,7 +305,7 @@ Renderer.prototype._renderPageContentSegmentImage = function _renderPageContentS
         });
 };
 
-Renderer.prototype._renderPageContentSegmentLines = function _renderPageContentSegmentLines(outputDirPath, siteDefiniton, pageDefinition, content){
+Renderer.prototype._renderPageContentSegmentLines = function _renderPageContentSegmentLines(outputDirPath, pageDefinition, content){
     let htmlArtifacts = [
         '<p class="ep-lines">',
     ];
@@ -269,11 +314,12 @@ Renderer.prototype._renderPageContentSegmentLines = function _renderPageContentS
     return htmlArtifacts.join('');
 };
 
-Renderer.prototype._renderPageContentSegmentParagraph = function _renderPageContentSegmentParagraph(outputDirPath, siteDefiniton, pageDefinition, content){
+Renderer.prototype._renderPageContentSegmentParagraph = function _renderPageContentSegmentParagraph(outputDirPath, pageDefinition, content){
     return `<p class="ep-paragraph">${escapeHtml(content.text)}</p>`;
 };
 
-Renderer.prototype.renderFooter = function renderFooter(outputDirPath, siteDefiniton, pageDefinition){
+Renderer.prototype.renderFooter = function renderFooter(outputDirPath, pageDefinition){
+    // TODO
 };
 
 if(require.main === module){
